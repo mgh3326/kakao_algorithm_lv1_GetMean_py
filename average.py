00cd36f19dd5c84e1d359692f8b6f0eae8ebfd4f
mgh3326 <mgh3326@naver.com>
2017-11-10 09:55:16 +0900

    complete

--- average.py
+++ average.py
@@ -1,9 +1,11 @@
 def average(list):
     # 함수를 완성해서 매개변수 list의 평균값을 return하도록 만들어 보세요.
-    total = 0
-    for i in list:
-        total += i
-    return total/len(list)
+    return (sum(list) / len(list))
+
+    # total = 0
+    # for i in list:
+    #     total += i
+    # return total/len(list)
 
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
 list = [5,3,4]
